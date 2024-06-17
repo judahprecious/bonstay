ad448dd4656710141a3a02684326c60050f636b3
judahprecious <judahprecious.j@gmail.com>
2024-06-17 11:10:34 +0530

    Started this project on 17th June

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Link, Routes } from "react-router-dom";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div>
+          <nav
+            data-testid="nav-bar"
+            className="navbar navbar-expand-lg navbar-light  bg-custom"
+          >
+            <Link className="nav-link" style={{ fontFamily: "cursive" }} to="">
+              BONSTAY
+            </Link>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="">
+                  Home
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" to="">
+                  Hotels
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">
+                  Bookings
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="">
+                  Logout
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>{/*configure the Route's */}</Routes>
+        </div>
+      </BrowserRouter>
+    
+    </>
   );
-}
+};
 
 export default App;
