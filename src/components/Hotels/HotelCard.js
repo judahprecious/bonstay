f6c3ab00719065567fdf1b5d38d4965281890262
judahprecious <judahprecious.j@gmail.com>
2024-06-20 16:51:06 +0530

    Add files via upload

--- src/components/Hotels/HotelCard.js
+++ src/components/Hotels/HotelCard.js
@@ -1,36 +1,18 @@
 
 const imageUrl = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/bf/06/59/killashee-hotel-spa-leisure.jpg?w=500&h=-1&s=1"
 
-const card = {
-    "id": 1,
-    "hotelName": "Paradise Stay",
-    "city": "Bangalore",
-    "description": "A home to relax at the 'city' peek with all the facilities near you",
-    "amenities": "Different Cuisine Food, Swimming Pool, Self Cooking Station",
-    "phoneNo": 9090909090,
-    "address": "120/1C, Bangalore, Karnataka",
-    "imageUrl": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/bf/06/59/killashee-hotel-spa-leisure.jpg?w=500&h=-1&s=1",
-    "reviews": [
-      "Good Accomodation",
-      "Best place to relax with friends and family",
-      "The best place to chill with friends. ",
-      "The best place I ever visited."
-    ] 
-
-};
-
-const HotelCard = () => {
+const HotelCard = ({card}) => {
     return (
         <div className="hotel_card">
 
-                <div className="image_container">
+                <div className="img_container">
                 <img src={imageUrl} alt="hotel room"/>
                     
                 </div>
 
 
                 <div className="details_container">
-                    <h4>{card.hotel}</h4>
+                    <h4>{card.hotelName}</h4>
                     <p>City: {card.city}</p>
                     <p>Amanetics:{card.amenities}</p>
                     <p>Address:{card.address}</p>
@@ -45,11 +27,9 @@ const HotelCard = () => {
                 <button>View Review</button>
 
                </div>
-                {/* <div className="reviews_container"></div> */}
-           
+              
 
 
-           
         </div>
     )
 }
--- src/components/Hotels/index.js
+++ src/components/Hotels/index.js
@@ -1,17 +1,28 @@
 import Header from "../CommonComponents/header";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Styles//hotels.css";
 import HotelCard from "./HotelCard";
+import axios from "axios";
 
 const Hotels = () => {
+    let a = 10;
+    const [hotelsData, setHotelsData] = useState();
+
+    useEffect(() => {
+        console.log("use effect is running");
+        axios.get("http://localhost:3000/hotels")
+        .then((res) => {
+            console.log(res.data);
+            setHotelsData(res.data);
+        });
+    );
     return (
         <div className="pages">
             <Header/>
             <main>
-                <HotelCard />
+                <HotelCard card={jay}/>
             </main>
         </div>
-        
 
     )
 }
